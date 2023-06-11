ef7ec24ea0d7b6907d914738f38fb9c444813824
Devansh Lodhi <1129.gh0st@gmail.com>
2023-06-11 16:36:36 +0530

    Updated

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+
 import {BsSearch} from 'react-icons/bs'
 import '../scss/components/_search.sass'
 import TrendContainer from './TrendContainer'
+import { useState } from 'react'
 
+interface Props {
+    size: string
+}
 
-const Search = () => {
+const Search = (props:Props) => {
+    const [show, setShow] = useState(false)
   return (
     <>
-        <div className='Search_Container1'>
+        <div className={`Search_Container1 ${props.size}`} onFocus={()=>setShow(true)} onBlur={()=>setShow(false)}>
             <input type="text" placeholder='Search' />
-            <span><BsSearch size={30} /></span>
+            <span><BsSearch size={28} /></span>
         </div>
-        <TrendContainer />
+        {
+            show && <TrendContainer />
+        }
+        
         
     </>
   )
---
+++ src/components/SearchContent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchContent = () => {
+  return (
+    <main className=''>
+        Content
+        </main>
+  )
+}
+
+export default SearchContent
